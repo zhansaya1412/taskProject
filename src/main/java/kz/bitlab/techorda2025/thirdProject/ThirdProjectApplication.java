6a90f4ae579308ffaf1af7bfc11f29f9f2b5e647
Zhansaya <133661007+zhansaya1412@users.noreply.github.com>
2025-02-13 20:27:04 +0500

    2025-02-13 added files for project

---
+++ src/main/java/kz/bitlab/techorda2025/thirdProject/ThirdProjectApplication.java
@@ -0,0 +1,13 @@
+package kz.bitlab.techorda2025.thirdProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/bitlab/techorda2025/thirdProject/ThirdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.bitlab.techorda2025.thirdProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
