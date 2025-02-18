58006cadcf620445eebf4d751577352461429968
Farabi Seiilbek <78350272+farabi23@users.noreply.github.com>
2025-02-18 20:28:47 +0500

    commit project

---
+++ TaskManager_Task/src/main/java/com/TaskManager/TaskManager/Task.java
@@ -0,0 +1,64 @@
+package com.TaskManager.TaskManager;
+
+public class Task {
+
+    private Long id;
+    private String name;
+    private String deadlineDate;
+
+    private String isCompleted;
+
+    private String description;
+
+    public Task() {
+
+    }
+
+    public Task(Long id, String name, String deadlineDate, String isCompleted, String description) {
+        this.id = id;
+        this.name = name;
+        this.deadlineDate = deadlineDate;
+        this.isCompleted = isCompleted;
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDeadlineDate() {
+        return deadlineDate;
+    }
+
+    public void setDeadlineDate(String deadlineDate) {
+        this.deadlineDate = deadlineDate;
+    }
+
+    public String getIsCompleted() {
+        return isCompleted;
+    }
+
+    public void setIsCompleted(String isCompleted) {
+        this.isCompleted = isCompleted;
+    }
+}
