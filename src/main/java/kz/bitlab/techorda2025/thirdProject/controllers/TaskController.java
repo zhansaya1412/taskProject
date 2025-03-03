9bb1e3d8f9ce316d72631809793230afe774a0a7
fwqerop <147277446+fwqerop@users.noreply.github.com>
2025-03-04 00:55:49 +0500

    change files

--- src/main/java/kz/bitlab/techorda2025/thirdProject/controllers/TaskController.java
+++ src/main/java/kz/bitlab/techorda2025/thirdProject/controllers/TaskController.java
@@ -1,9 +1,10 @@
 package kz.bitlab.techorda2025.thirdProject.controllers;
 
+import kz.bitlab.techorda2025.thirdProject.db.Task;
 import kz.bitlab.techorda2025.thirdProject.db.TaskManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class TaskController {
@@ -14,4 +15,35 @@ public class TaskController {
         return "tasks";
     }
 
+    @GetMapping("/details/{id}")
+    public String getDetails(@PathVariable(name = "id") Long id, Model model){
+        model.addAttribute("task", TaskManager.getTask(id));
+        return "details";
+    }
+
+    @PostMapping("/saveTask")
+    public String saveTask(
+            @RequestParam(name = "taskId") Long  id,
+            @RequestParam(name = "taskName") String name,
+            @RequestParam(name = "taskDescription") String description,
+            @RequestParam(name = "task_deadline") String deadline,
+            @RequestParam(name = "task_status") String status
+            )
+    {
+        Task task = TaskManager.getTask(id);
+        task.setId(id);
+        task.setName(name);
+        task.setDescription(description);
+        task.setDeadlineDate(deadline);
+        task.setCompleted(Boolean.parseBoolean(status));
+
+        return "redirect:/tasks";
+    }
+
+    @PostMapping("/deleteTask")
+    public String deleteTask(@RequestParam(name = "taskId") Long id){
+        TaskManager.deleteTask(id);
+
+        return "redirect:/tasks";
+    }
 }
--- src/main/java/kz/bitlab/techorda2025/thirdProject/db/TaskManager.java
+++ src/main/java/kz/bitlab/techorda2025/thirdProject/db/TaskManager.java
@@ -12,11 +12,24 @@ public class TaskManager {
     public static List<Task> taskList = new ArrayList<>();
 
     static {
-        taskList.add(new Task(1L, "Congratulate father", "2025-02-25", false));
-        taskList.add(new Task(2L, "Clean the room", "2025-03-08", false));
-        taskList.add(new Task(3L, "To eat", "2025-03-03", true));
-        taskList.add(new Task(4L, "Understand git and github", "2025-03-04", false));
-        taskList.add(new Task(5L, "To do assignment 3", "2025-03-03", false));
+        taskList.add(new Task(1L, "Congratulate father", "Call to dad at midnight", "2025-02-25", false));
+        taskList.add(new Task(2L, "Clean the room", "Do not be lazy!", "2025-03-08", false));
+        taskList.add(new Task(3L, "To eat", "Make a dinner (desirable)", "2025-03-03", true));
+        taskList.add(new Task(4L, "Understand git and github", "As soon as possible", "2025-03-04", false));
+        taskList.add(new Task(5L, "To do assignment 3", "The deadline is in 2 hours!!!", "2025-03-03", false));
     }
 
+    public static Task getTask(Long id){
+        return taskList.stream().filter(task -> task.getId().equals(id)).findFirst().orElse(null);
+    }
+
+    public static void addTask(Task task){
+        task.setId(id);
+        taskList.add(task);
+        id++;
+    }
+
+    public static void deleteTask(Long id) {
+        taskList.remove(TaskManager.getTask(id));
+    }
 }
